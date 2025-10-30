b1950a87676f3c4cf94d87e72bf53d19d303ec93
lvtwna1210 <lvtwna12@gmail.com>
2025-10-31 01:25:30 +0700

    Create AdminMenuComponent.cs

---
+++ Areas/Admin/Components/AdminMenuComponent.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project.Areas.Admin.Components
+{
+    [ViewComponent(Name = "AdminMenu")]
+    public class AdminMenuComponent : ViewComponent
+    {
+        private readonly DataContext _context;
+        public AdminMenuComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var mnList = (from mn in _context.AdminMenus
+                          where (mn.IsActive == true)
+                          select mn).ToList();
+            return await Task.FromResult((IViewComponentResult)View("Default", mnList));
+        }
+    }
+}
